972a3822e8cc6da29bd43fe2cf5006817ee89811
c0pper <93simonster@gmail.com>
2022-10-17 23:26:41 +0200

    custom

--- config.js
+++ config.js
@@ -1,107 +1,57 @@
 // Username displayed in greeting.
-var userName = "gabeweb";
+var userName = "simone";
 
 // Default Searchengine
 // Available: Bing DuckDuckGo Google Yahoo Yandex
-var searchEngine = "DuckDuckGo";
+var searchEngine = "Google";
 
 // Cards
 var cards = [
 	{
-		name: "Email",
+		name: "Google",
 		bookmarks: {
-			"Aol Mail": "https://mail.aol.com",
 			"Google Mail": "https://mail.google.com",
-			"OnMail": "https://mail.onmail.com",
-			"Outlook": "https://mail.live.com",
-			"Proton Mail": "https://mail.proton.me",
-			"Yahoo Mail": "https://mail.yahoo.com",
-			"Yandex Mail": "https://mail.yandex.com"
+			"Proton Mail": "https://mail.proton.me/u/0/inbox?welcome=true",
+			"Photos": "https://photos.google.com/",
+			"Drive": "https://drive.google.com/drive/my-drive",
+			"Maps": "https://www.google.it/maps/@40.842859,14.2512226,15z",
+			"Keep": "https://keep.google.com/"
 		}
 	},
 	{
-		name: "News",
+		name: "Stupid shit",
 		bookmarks: {
-			"AP News":"https://apnews.com",
-			"Ars Technica": "https://arstechnica.com",
-			"BBC News": "https://bbc.com",
-			"Boing Boing": "https://boingboing.net",
-			"CNET": "https://cnet.com",
-			"Consequence": "https://consequence.net",
-			"Digg": "https://digg.com",
-			"Forbes": "https://forbes.com",
-			"Mashable": "https://mashable.com",
-			"Newsweek":"https://newsweek.com",
-			"NY Times": "https://nytimes.com",
-			"NPR": "https://npr.org",
-			"Pitchfork": "https://pitchfork.com",
-			"Quartz":"https://qz.com",
-			"Reuters":"https://reuters.com",
-			"The Guardian":"https://theguardian.com",
-			"The Verge": "https://theverge.com",
-			"TIME":"https://time.com",
-			"TNW": "https://thenextweb.com",
-			"Vox":"https://vox.com",
-			"XDA": "https://xda-developers.com",
-			"ZDNET": "https://zdnet.com"
+			"Reddit":"http://reddit.com/",
+			"Starship Dev": "https://reddit-stream.com/comments/xzbhus/",
+			"Youtube": "http://www.youtube.com/index?gl=IT",
+			"Discord": "https://discordapp.com/channels/@me",
+			"4chan": "https://4chan.org/",
+			"Twitter": "https://twitter.com"
 		}
 	},
 	{
-		name: "Social",
+		name: "Content",
 		bookmarks: {
-			"DEV": "https://dev.to",
-			"Discord": "https://discordapp.com",
-			"Facebook": "https://facebook.com",
-			"Instagram": "https://instagram.com/?theme=dark",
-			"Messenger": "https://messenger.com",
-			"Quora": "https://quora.com",
-			"Reddit": "https://reddit.com",
-			"Stack Exchange": "https://stackexchange.com",
-			"Telegram": "https://web.telegram.org",
-			"Tumblr": "https://tumblr.com",
-			"Twitter": "https://twitter.com",
-			"VK": "https://vk.com",
-			"WhatsApp": "https://web.whatsapp.com"
-		}
-	},
-	{
-		name: "Media",
-		bookmarks: {
-			"Behance": "https://behance.net",
-			"Crumchyroll": "https://crunchyroll.com",
-			"Deviantart": "https://deviantart.com",
-			"Dribbble": "https://dribbble.com",
-			"Ello": "https://ello.co",
-			"Last.fm": "https://last.fm",
-			"Pluto TV": "https://pluto.tv",
-			"Runtime": "https://runtime.tv",
-			"Spotify": "https://open.spotify.com",
-			"Trakt.tv": "https://trakt.tv",
-			"YouTube": "https://youtube.com",
-			"YouTube Music": "https://music.youtube.com"
+			"Plex": "https://app.plex.tv/desktop/#!/",
+			"AniChart": "https://anichart.net/airing",
+			"Documentaries": "https://ihavenotv.com/",
+			"NSW2U": "https://nsw2u.xyz/",
+			"Fitgirl": "https://fitgirl-repacks.site/",
+			"CS-RIN": "https://cs.rin.ru/forum/",
+			"RARBG": "https://rarbg.to/index80.php",
+			"1337x": "https://www.1377x.to/",
+			"Nyaa": "https://nyaa.si/"
 		}
 	},
 	{
 		name: "Tools",
 		bookmarks: {
 			"Bitwarden": "https://vault.bitwarden.com",
-			"Coda": "https://coda.io",
 			"Dropbox": "https://dropbox.com",
-			"Figma": "https://figma.com",
 			"GitHub": "https://github.com",
-			"Google Docs": "https://docs.google.com",
-			"Google Drive": "https://drive.google.com",
-			"Google Passwords": "https://passwords.google.com",
-			"Google Translate": "https://translate.google.com",
-			"KeeWeb": "https://app.keeweb.info",
-			"Notion": "https://notion.so",
-			"Office 365": "https://office.com",
 			"OneDrive": "https://onedrive.com",
 			"Photopea": "https://photopea.com",
-			"VSCode": "https://vscode.dev",
-			"Vectr": "https://vectr.comm",
-			"Write.as": "https://write.as",
-			"Yandex Disk": "https://disk.yandex.com"
+			"VSCode": "https://vscode.dev"
 		}
 	}
 ]
